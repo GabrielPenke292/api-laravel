1727302182eab3008809bdd46511c6b83578f325
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-03 20:09:04 -0300

    using prefix 'v1'

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,6 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 
-Route::get("/users", [UserController::class, "index"]);
+Route::prefix("v1")->group(function () {
+    Route::get("/users", [UserController::class, "index"]);
+});
